5e595f8ffc8f8b934f689b74608a945b10b194df
Jacob <jacob.dorpinghaus@gmail.com>
2017-04-05 22:33:06 -0500

    Finished lab

--- lab8/Debug/src/lab8.d
+++ lab8/Debug/src/lab8.d
@@ -1,1 +1,1 @@
-src/lab8.d: ../src/lab8.cpp
+src/lab8.o: ../src/lab8.cpp
--- lab8/src/lab8.cpp
+++ lab8/src/lab8.cpp
@@ -13,8 +13,8 @@ class message{
 public:
 	message();
 	message(string messageString);
-	~message();
-	void print();
+	virtual ~message();
+	virtual void print();
 protected:
 	string messageText;
 };
@@ -23,46 +23,83 @@ class morseCodeMessage: public message{
 public:
 	morseCodeMessage();
 	morseCodeMessage(string messageString);
-	void print();
+	~morseCodeMessage();
+	virtual void print();
 private:
 	void translate();
 	string messageCode;
 };
 
-string morse[26] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
-"..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
-"...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+class messageStack{
+public:
+	messageStack(message* firstMessage);
+	~messageStack();
+	void push(message* messageObject);
+	message* pop(void);
+	void printStack(void);
+private:
+	vector <message*> stack;
+};
 
 int main() {
-	morseCodeMessage morse1();
+	message m1("message1");
+	message m2;
+	morseCodeMessage morse1("morse code message one");
+	morseCodeMessage morse2;
+
+	cout << "\n\n$$$$$$$$$$$$ MESSAGE 1 $$$$$$$$$$$$" << endl;
+	m1.print();
+	cout << "$$$$$$$$$$$$ MESSAGE 2 $$$$$$$$$$$$" << endl;
+	m2.print();
+
+
+	cout << "\n\n$$$$$$$$$$$$ MORSE MESSAGE 1 $$$$$$$$$$$$" << endl;
+	morse1.print();
+	cout << "$$$$$$$$$$$$ MORSE MESSAGE 2 $$$$$$$$$$$$" << endl;
+	morse2.print();
+
+	cout << "\n\n$$$$$$$$$$$$ MESSAGE STACK $$$$$$$$$$$$" << endl;
+	messageStack stack(&m1);
+	stack.push(&m2);
+	stack.push(&morse1);
+	stack.push(&morse2);
+	stack.printStack();
+
+	message* poppedMessage = stack.pop();
+	cout << "\n\n$$$$$$$$$$$$ POPPED MESSAGE $$$$$$$$$$$$" << endl;
+	poppedMessage->print();
+
+	cout << "\n\n$$$$$$$$$$$$ MESSAGE STACK AFTER POP $$$$$$$$$$$$" << endl;
+	stack.printStack();
+
 	return 0;
 }
 
 message::message(){
-	cout << "Enter your message: " << endl;
-	cin >> messageText;
+	cout << "\nEnter your message: " << endl;
+	getline(cin, messageText, '\n');
 }
 
-message::~message(){
+message::~message(){ //nothing to free
 }
 
 message::message(string messageString){
-
+	messageText = messageString;
 }
 
 void message::print(){
 	cout << messageText << endl;
 }
 
-morseCodeMessage::morseCodeMessage(string messageString){
-	messageText = messageString;
+morseCodeMessage::morseCodeMessage(string messageString) : message(messageString){ //calls message constructor to set message parameter
 	translate();
 }
 
 morseCodeMessage::morseCodeMessage(){
-	cout << "Enter your message: " << endl;
-	cin >> messageText;
 	translate();
+}
+
+morseCodeMessage::~morseCodeMessage(){ //nothing to free
 }
 
 void morseCodeMessage::print(void){
@@ -71,14 +108,74 @@ void morseCodeMessage::print(void){
 }
 
 void morseCodeMessage::translate(){
+	string morse[26] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+	"..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+	"...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."}; //only alpha characters supported with this array
 	unsigned int i;
+	string tempString;
 	int position;
 	for(i=0;i<messageText.length();i++){
-		if(!isalpha(messageText[i])){
-			cout << "Error: only alpha characters allowed to be translated" << endl;
-			exit(0);
+		//if(!isalpha(messageText[i])){
+		//	cout << "Error: only alpha characters allowed to be translated" << endl;
+		//	exit(0);
+		//}
+		if(islower(messageText[i])){ //handle lowercase letters
+			position = messageText[i] - 'a';
+			messageCode += morse[position];
+		} else if(isupper(messageText[i])){ //handle capital letters
+			position = messageText[i] - 'A';
+			messageCode += morse[position];
+		} else{
+			switch(messageText[i]){ //special cases for handling punctuation
+			case '.':
+				tempString = ".-.-.-";
+				break;
+			case ',':
+				tempString = "--..--";
+				break;
+			case '?':
+				tempString = "..--..";
+				break;
+			case ' ':
+				tempString = " ";
+				break;
+			case '!':
+				tempString = "-.-.--";
+				break;
+			case '\'':
+				tempString = ".----.";
+				break;
+			default:
+				cout << "ERROR invalid character \"" << messageText[i] << "\"" << endl; //only alpha characters and some punctuation supported
+				exit(0);
+			}
+			messageCode += tempString;
 		}
-		position = messageText[i] - 'a';
-		messageCode += morse[position];
+	}
+}
+
+messageStack::messageStack(message* firstMessage){
+	stack.push_back(firstMessage);
+}
+
+messageStack::~messageStack(){
+}
+
+void messageStack::push(message* newMessage){
+	stack.push_back(newMessage);
+}
+
+message* messageStack::pop(void){
+	message* tempMessage;
+	tempMessage = stack.back();
+	stack.pop_back();
+	return tempMessage;
+}
+
+void messageStack::printStack(void){
+	int i;
+	int size = stack.size();
+	for(i=1;i<=size;i++){
+		stack[size-i]->print();
 	}
 }
